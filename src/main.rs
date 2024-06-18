2125218dac8a071bb00fe56c1e8ce4774165d1dd
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-06-18 13:45:35 +0530

    respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 use anyhow::Result;
-use std::net::TcpListener;
+use bytes::Bytes;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:4221")?;
+    let listener = TcpListener::bind("127.0.0.1:4221").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+    loop {
+        match listener.accept().await {
+            Ok((stream, _socketaddr)) => {
                 println!("accepted new connection");
+                handle_http_response(stream).await?;
             }
             Err(e) => {
                 println!("error: {}", e);
+                break;
             }
         }
     }
+
+    Ok(())
+}
+
+async fn handle_http_response(mut stream: TcpStream) -> Result<()> {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next_line().await?.unwrap();
+
+    let status_line = match request_line.as_str() {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "",
+    };
+
+    let response = Bytes::from(status_line);
+
+    stream.write_all(&response).await?;
 
     Ok(())
 }
